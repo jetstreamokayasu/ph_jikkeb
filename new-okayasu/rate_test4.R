4daf7ae1a65a4de759b052ee13693453fb9dd7d0
jetstreamokayasu <tokotsuku2@outlook.jp>
2019-07-08 22:15:19 +0900

    340点トーラス計算し直し

--- new-okayasu/rate_test4.R
+++ new-okayasu/rate_test4.R
@@ -65,4 +65,58 @@ torus_colset1_aggrs<-lapply(1:4, function(k){
   return(append(aggr, list(time=time)))
 
 })
+save2Rdata(torus_colset1_aggrs)
+
+
+#340点トーラス5セット
+torus340_colle_set<-lapply(1:5, function(j){
+  
+  torus_collect<- lapply(1:100, function(i){
+    nsample <- 340
+    #var <- runif(1, var.min, var.max)
+    #noize.torus <- matrix(rnorm(nsample * 3, 0, var), nrow = nsample)
+    torus <- torusUnif(nsample, 1, 2.5)
+    return(list(nsample = nsample, noizyX = torus, diag = 0))
+  })
+  
+  return(torus_collect)
+  
+})
+save2Rdata(torus340_colle_set)
+
+torus340_colset_aggrs<-lapply(1:5, function(k){
+  
+  cat("list", k, "calc\n")
+  time<-system.time(aggr<-proposedMethodOnly(torus340_colle_set[[k]], 2, 3, 10))
+  return(append(aggr, list(time=time)))
+  
+})
+save2Rdata(torus340_colset_aggrs)
+
+
+#330点トーラス5セット
+torus330_colle_set<-lapply(1:5, function(j){
+  
+  torus_collect<- lapply(1:100, function(i){
+    nsample <- 330
+    #var <- runif(1, var.min, var.max)
+    #noize.torus <- matrix(rnorm(nsample * 3, 0, var), nrow = nsample)
+    torus <- torusUnif(nsample, 1, 2.5)
+    return(list(nsample = nsample, noizyX = torus, diag = 0))
+  })
+  
+  return(torus_collect)
+  
+})
+save2Rdata(torus340_colle_set)
+
+torus340_colset_aggrs<-lapply(1:5, function(k){
+  
+  cat("list", k, "calc\n")
+  time<-system.time(aggr<-proposedMethodOnly(torus340_colle_set[[k]], 2, 3, 10))
+  return(append(aggr, list(time=time)))
+  
+})
+save2Rdata(torus340_colset_aggrs)
+
 
