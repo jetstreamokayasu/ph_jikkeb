879b1435f149426a86ca295fa1d1376ba3f1dc27
jetstreamokayasu <tokotsuku2@outlook.jp>
2019-07-19 08:03:08 +0900

    アクロボットのPDを計算。トーラス320点補間後実験開始

--- new-okayasu/rate_test4.R
+++ new-okayasu/rate_test4.R
@@ -153,10 +153,10 @@ torus330_colle_set<-lapply(1:5, function(j){
 })
 save2Rdata(torus330_colle_set)
 
-torus340_colset_aggrs<-lapply(1:5, function(k){
+torus330_colset_aggrs<-lapply(1:5, function(k){
   
   cat("list", k, "calc\n")
-  time<-system.time(aggr<-proposedMethodOnly(torus340_colle_set[[k]], 2, 3, 10))
+  time<-system.time(aggr<-proposedMethodOnly(torus330_colle_set[[k]], 2, 3, 10))
   return(append(aggr, list(time=time)))
   
 })
@@ -177,6 +177,51 @@ torus330_incolle13_aggrs<-lapply(1:3, function(k){
   
 })
 save2Rdata(torus330_incolle13_aggrs)
+
+
+
+#320点トーラス5セット
+torus320_colle_set<-lapply(1:5, function(j){
+  
+  torus_collect<- lapply(1:100, function(i){
+    nsample <- 320
+    #var <- runif(1, var.min, var.max)
+    #noize.torus <- matrix(rnorm(nsample * 3, 0, var), nrow = nsample)
+    torus <- torusUnif(nsample, 1, 2.5)
+    return(list(nsample = nsample, noizyX = torus, diag = 0))
+  })
+  
+  return(torus_collect)
+  
+})
+save2Rdata(torus320_colle_set)
+
+torus320_colset_aggrs<-lapply(1:5, function(k){
+  
+  cat("list", k, "calc\n")
+  time<-system.time(aggr<-proposedMethodOnly(torus320_colle_set[[k]], 2, 3, 10))
+  return(append(aggr, list(time=time)))
+  
+})
+save2Rdata(torus320_colset_aggrs)
+
+#320点トーラス補間後
+t320_intime<-system.time(torus320_incolle_set<-lapply(torus320_colle_set, function(k)all_interpolate(k, 15)))
+save2Rdata(t320_intime)
+
+save2Rdata(torus320_incolle_set)
+
+##330点トーラス補間後1~3セット目を推定
+torus320_incolle13_aggrs<-lapply(1:3, function(k){
+  
+  cat("list", k, "calc\n")
+  time<-system.time(aggr<-proposedMethodOnly(torus320_incolle_set[[k]], 2, 3, 10))
+  return(append(aggr, list(time=time)))
+  
+})
+save2Rdata(torus320_incolle13_aggrs)
+
+
 
 #300点トーラス5セット
 torus300_colle_set<-lapply(1:5, function(j){
@@ -219,4 +264,4 @@ points3d(intrs300_2[[1]][[2]][301:487, ], col=2)
 nsample_intrs300_2<-lapply(intrs300_2, function(intrs){intrs[["nsample"]]})
 
 
-#アクロボットを作る
+#
