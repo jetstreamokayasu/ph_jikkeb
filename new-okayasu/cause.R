cb4f8704b2203a989908deb6e5efc904165c7392
jetstreamokayasu <tokotsuku2@outlook.jp>
2019-08-09 20:38:42 +0900

    推定成功率グラフを作成

--- new-okayasu/cause.R
+++ new-okayasu/cause.R
@@ -13,23 +13,26 @@ library(interpo3d)
 #torus.collect18とtorus_colle_set1が350点の補間前トーラス
 
 #補間前に正解なのと補間後に正解しなかったデータセット比較
-crrct1_10<-which(torus_colset1_aggrs[[1]][[1]] >= 1.5 & torus_colset1_aggrs[[1]][[1]] < 2.5)[1:10]
+crrct1_10<-which(torus_colset1_aggrs[[1]][[1]] >= 1.5 & torus_colset1_aggrs[[1]][[1]] < 2.5)
 
-inwrng1_10<-which(torus350_incolle_aggrs[[2]][[1]] < 1.5 | torus_colset1_aggrs[[1]][[1]] >= 2.5)[1:10]
+inwrng1_10<-which(torus350_incolle_aggrs[[2]][[1]] < 1.5 | torus_colset1_aggrs[[1]][[1]] >= 2.5)
+
+c_inw_1_10<-intersect(crrct1_10, inwrng1_10)[1:10]
+
+trs350_2_cw1_10_pd<-lapply(c_inw_1_10, function(k)ripsDiag(torus350_colle_set[[2]][[k]][["noizyX"]], 2, 3, printProgress = T))
+plotPDs(trs350_2_cw1_10_pd)
+
+trs350_2_cw1_10_pls<-lapply(1:10, function(k)calcLandscape(trs350_2_cw1_10_pd[[k]]))
+plot_lands(trs350_2_cw1_10_pls, 1)
+
+trs_in350_2_cw1_10_pd<-lapply(c_inw_1_10, function(k)ripsDiag(torus350_incolle_set[[2]][[k]][["noizyX"]], 2, 3, printProgress = T))
+plotPDs(trs_in350_2_cw1_10_pd)
+
+trs_in350_2_cw1_10_pls<-lapply(1:10, function(k)calcLandscape(trs_in350_2_cw1_10_pd[[k]]))
+plot_lands(trs_in350_2_cw1_10_pls, 1)
 
 
-trs350_2_w1_10_pd<-lapply(inwrng1_10, function(k)ripsDiag(torus350_colle_set[[2]][[k]][["noizyX"]], 2, 3, printProgress = T))
-plotPDs(trs350_2_w1_10_pd)
-
-trs350_2_w1_10_pls<-lapply(1:10, function(k)calcLandscape(trs350_2_w1_10_pd[[k]]))
-plot_lands(trs350_2_w1_10_pls, 1)
-
-trs_in350_2_w1_10_pd<-lapply(inwrng1_10, function(k)ripsDiag(torus350_incolle_set[[2]][[k]][["noizyX"]], 2, 3, printProgress = T))
-plotPDs(trs_in350_2_w1_10_pd)
-
-trs_in350_2_w1_10_pls<-lapply(1:10, function(k)calcLandscape(trs_in350_2_w1_10_pd[[k]]))
-plot_lands(trs_in350_2_w1_10_pls, 1)
-
+#新手法の補間の誤差を調べる
 trs_in350_2_26_der<-torus_disterror(torus350_incolle_set[[2]][[26]][["noizyX"]], maxr = 2.5, minr = 1, nps = 350)
 hist(trs_in350_2_26_der, col="#993435")
 
@@ -42,7 +45,7 @@ boxplot(trs_in350_1_w1_10_ders, xlab="Data Set", ylab="Error", cex.lab=1.6, cex.
 
 trs_in300_1_1_10_ders<-lapply(1:10, function(i)torus_disterror(torus300_1_1[[i]][["noizyX"]], maxr = 2.5, minr = 1, nps = 300))
 par(mgp=c(2.5,1,0))
-boxplot(trs_in350_1_w1_10_ders, xlab="Data Set", ylab="Error", cex.lab=1.6, cex.axis=1.6)
+boxplot(trs_in300_1_1_10_ders, xlab="Data Set", ylab="Error", cex.lab=1.6, cex.axis=1.6)
 
 
 #サブサンプルのPDを確かめる
@@ -102,3 +105,11 @@ torus300_incolle_1_a6_2<-variable_interpo(torus300_colle_set[[1]], 15, 6)
 intt300_1_a3_time<-system.time(trs300_incolle1_a3_aggr<-proposedMethodOnly(torus300_incolle_1_a3, 2, 3, 10))
 
 intt300_1_a6_time<-system.time(trs300_incolle1_a6_aggr<-proposedMethodOnly(torus300_incolle_1_a6, 2, 3, 10))
+
+
+torus350<-torus350_incolle_set[[2]][[26]][["noizyX"]] %>%
+  dplyr::as_data_frame() %>% 
+  cbind(., c(rep(1, 300), rep(2, 279)))
+colnames(torus350)[4]<-"inter"
+plotly::plot_ly(torus350, x = ~x, y = ~y, z = ~z, size = 1, color= ~inter, colors = c('#BF382A', '#0C4B8E')) %>% 
+plotly::add_markers()
--- new-okayasu/rate_test4.R
+++ new-okayasu/rate_test4.R
@@ -78,6 +78,11 @@ save2Rdata(t350_intime)
 torus350_incolle_set_5<-all_interpolate(collect = torus.collect18, nvic = 15)
 save2Rdata(torus350_incolle_set)
 
+figurePlot(torus350_incolle_set_5[[1]][["noizyX"]][1:350,])
+rgl.snapshot("./data/trs350_colle_set_5_1.png")
+points3d(torus350_incolle_set_5[[1]][["noizyX"]][351:556,], col=2)
+rgl.snapshot("./data/trs350_incolle_set_5_1.png")
+
 torus350_colle_set<-append(torus_colle_set1, list(torus.collect18))
 save2Rdata(torus350_colle_set)
 
@@ -259,6 +264,7 @@ torus310_incolle13_aggrs<-lapply(1:3, function(k){
   
   cat("list", k, "calc\n")
   time<-system.time(aggr<-proposedMethodOnly(torus310_incolle_set[[k]], 2, 3, 10))
+  save(list = aggr, file = paste0("./data/in310_aggr_", k, ".RData"))
   return(append(aggr, list(time=time)))
   
 })
@@ -307,4 +313,150 @@ points3d(intrs300_2[[1]][[2]][301:487, ], col=2)
 nsample_intrs300_2<-lapply(intrs300_2, function(intrs){intrs[["nsample"]]})
 
 
-#
+#推定成功率まとめ
+#2次ベッチ数
+#補間後
+torus350_incolle_rate<-aggr_success_rates(torus350_incolle_aggrs, c(2,1))
+torus340_incolle13_rate<-aggr_success_rates(torus340_incolle13_aggrs, c(2,1))
+torus330_incolle13_rate<-aggr_success_rates(torus330_incolle13_aggrs, c(2,1))
+torus320_incolle13_rate<-aggr_success_rates(torus320_incolle13_aggrs, c(2,1))
+torus300_1_1_rate<-aggr_success_rates(list(intrs300_1_aggr), c(2,1))
+
+in350_rates<-do.call(rbind, torus350_incolle_rate)
+in340_rates<-do.call(rbind, torus340_incolle13_rate)
+in330_rates<-do.call(rbind, torus330_incolle13_rate)
+in320_rates<-do.call(rbind, torus320_incolle13_rate)
+
+in300_1_rate<-do.call(rbind, torus300_1_1_rate)
+
+insub_rates<-list("300"=in300_1_rates,
+                  "320"=in320_rates,
+                  "330"=in330_rates,
+                  "340"=in340_rates,
+                  "350"=in350_rates)
+
+points(rep(300, 1), in300_1_rates[,2], col=2, pch=16)
+points(rep(310, 5), insub310.rate, col=2, pch=16)
+points(rep(320, 3), in320_rates[,2], col=2, pch=16)
+points(rep(330, 3), in330_rates[,2], col=2, pch=16)
+points(rep(340, 3), in340_rates[,2], col=2, pch=16)
+points(rep(350, 5), in350_rates[,2], col=2, pch=16)
+
+in_dim2_mean<-sapply(insub_rates, function(rate)mean(unlist(rate[,2])))
+lines(c(300, seq(320, 350, by=10)), in_dim2_mean, col=2)
+
+in_dim2_sd<-sapply(insub_rates, function(rate)sd(unlist(rate[,2])))
+lines(c(300, seq(320, 350, by=10)), in_dim2_mean-in_dim2_sd, lty="dashed", col=2)
+lines(c(300, seq(320, 350, by=10)), in_dim2_mean+in_dim2_sd, lty="dashed", col=2)
+
+
+#1次ベッチ数
+#補間前
+suc300_rates<-aggr_success_rates(torus15.300subs.aggrs, c(2,1)) %>% do.call(rbind, .)
+suc310_rates<-aggr_success_rates(torus15.310subs.aggrs, c(2,1)) %>% do.call(rbind, .)
+suc320_rates<-do.call(cbind, torus15.320rates[["320sub"]])
+suc330_rates<-do.call(cbind, torus15.330rates[["330sub"]])
+suc340_rates<-do.call(cbind, torus15.340rates[["340sub"]])
+#suc350_rates<-do.call(cbind, torus15.350rates[["350sub"]])
+        
+suc350_rates_dim1<-sapply(torus15.350rates[["350sub"]][["dim1"]], function(rate){return(rate[1])})
+suc350_rates_dim2<-sapply(torus15.350rates[["350sub"]][["dim2"]], function(rate){return(rate[1])})
+
+suc350_rates<-data.frame(dim1rate=suc350_rates_dim1, dim2rate=suc350_rates_dim2)
+
+sub_rates<-list("300"=suc300_rates,
+                "310"=suc310_rates,
+                "320"=suc320_rates,
+                "330"=suc330_rates,
+                "340"=suc340_rates,
+                "350"=suc350_rates)
+
+
+plot(sucrate.dim2.tidy_2, pch=16, cex.axis=1.6, xlab="Data Density", ylab="Success Rates", cex.lab=1.6, ylim=c(0.2, 1.0), xaxt="n", type="n")
+axis(side=1, at=seq(300, 350, by=10), labels=c(paste0(seq(30, 35), "/(pi^2)")), cex.axis=1.1)
+
+points(rep(300, 5), suc300_rates[,1], pch=16)
+points(rep(310, 5), suc310_rates[,1], pch=16)
+points(rep(320, 5), suc320_rates[,1], pch=16)
+points(rep(330, 5), suc330_rates[,1], pch=16)
+points(rep(340, 5), suc340_rates[,1], pch=16)
+points(rep(350, 5), suc350_rates[,1], pch=16)
+
+dim1_mean<-sapply(sub_rates, function(rate)mean(unlist(rate[,1])))
+lines(seq(300, 350, by=10), dim1_mean)
+
+dim1_sd<-sapply(sub_rates, function(rate)sd(unlist(rate[,1])))
+lines(seq(300, 350, by=10), dim1_mean-dim1_sd, lty="dashed")
+lines(seq(300, 350, by=10), dim1_mean+dim1_sd, lty="dashed")
+
+#補間後
+points(rep(300, 1), in300_1_rates[,1], col=2, pch=16)
+points(rep(310, 5), insub310.rate, col=2, pch=16)
+points(rep(320, 3), in320_rates[,1], col=2, pch=16)
+points(rep(330, 3), in330_rates[,1], col=2, pch=16)
+points(rep(340, 3), in340_rates[,1], col=2, pch=16)
+points(rep(350, 5), in350_rates[,1], col=2, pch=16)
+
+in_dim1_mean<-sapply(insub_rates, function(rate)mean(unlist(rate[,1])))
+lines(c(300, seq(320, 350, by=10)), in_dim1_mean, col=2)
+
+in_dim1_sd<-sapply(insub_rates, function(rate)sd(unlist(rate[,1])))
+lines(c(300, seq(320, 350, by=10)), in_dim1_mean-in_dim1_sd, lty="dashed", col=2)
+lines(c(300, seq(320, 350, by=10)), in_dim1_mean+in_dim1_sd, lty="dashed", col=2)
+
+#旧手法
+#1次ベッチ数補間後
+
+insub300_rate13<-aggr_success_rates(list(torus15.300insubs1.aggr, torus15.300insubs2.aggr, torus15.300insubs3.aggr), c(2,1))
+
+insub300.rate<-append(insub300_rate13, torus15.300insubs4_5rate) %>% do.call(rbind, .)
+
+insub310.rate<-aggr_success_rates(c(torus15.310insubs1_3.aggrs, torus15.310insubs4_5.aggrs), c(2,1)) %>% do.call(rbind, .)
+
+insub320.rate<-aggr_success_rates(c(torus15.320insubs1_3.aggrs, torus15.320insubs4_5.aggrs), c(2,1)) %>% do.call(rbind, .)
+
+insub330.rate<-aggr_success_rates(c(torus15.330insubs1_3.aggrs, torus15.330insubs4_5.aggrs), c(2,1)) %>% do.call(rbind, .)
+
+insub340.rate<-aggr_success_rates(c(torus15.340insubs1_3.aggrs, torus15.340insubs4_5.aggrs), c(2,1)) %>% do.call(rbind, .)
+
+insub350.rate<-aggr_success_rates(c(torus15.350insubs1_3.aggrs, torus15.350insubs4_5.aggrs), c(2,1)) %>% do.call(rbind, .)
+
+
+insub.rates<-list("300"=insub300.rate,
+                  "310"=insub310.rate,
+                  "320"=insub320.rate,
+                  "330"=insub330.rate,
+                  "340"=insub340.rate,
+                  "350"=insub350.rate)
+
+save2Rdata(insub.rates)
+
+points(rep(300, 5), insub300.rate[,1], col=4, pch=16)
+points(rep(310, 5), insub310.rate[,1], col=4, pch=16)
+points(rep(320, 5), insub320.rate[,1], col=4, pch=16)
+points(rep(330, 5), insub330.rate[,1], col=4, pch=16)
+points(rep(340, 5), insub340.rate[,1], col=4, pch=16)
+points(rep(350, 5), insub350.rate[,1], col=4, pch=16)
+
+insubdim1.mean<-sapply(insub.rates, function(rate)mean(unlist(rate[,1])))
+lines(seq(300, 350, by=10), insubdim1.mean, col=4)
+
+insubdim1.sd<-sapply(insub.rates, function(rate)sd(unlist(rate[,1])))
+lines(seq(300, 350, by=10), insubdim1.mean-insubdim1.sd, lty="dashed", col=4)
+lines(seq(300, 350, by=10), insubdim1.mean+insubdim1.sd, lty="dashed", col=4)
+
+#旧手法
+#2次ベッチ数補間後
+points(rep(300, 5), insub300.rate[,2], col=4, pch=16)
+points(rep(310, 5), insub310.rate[,2], col=4, pch=16)
+points(rep(320, 5), insub320.rate[,2], col=4, pch=16)
+points(rep(330, 5), insub330.rate[,2], col=4, pch=16)
+points(rep(340, 5), insub340.rate[,2], col=4, pch=16)
+points(rep(350, 5), insub350.rate[,2], col=4, pch=16)
+
+insubdim2.mean<-sapply(insub.rates, function(rate)mean(unlist(rate[,2])))
+lines(seq(300, 350, by=10), insubdim2.mean, col=4)
+
+insubdim2.sd<-sapply(insub.rates, function(rate)sd(unlist(rate[,2])))
+lines(seq(300, 350, by=10), insubdim2.mean-insubdim2.sd, lty="dashed", col=4)
+lines(seq(300, 350, by=10), insubdim2.mean+insubdim2.sd, lty="dashed", col=4)
