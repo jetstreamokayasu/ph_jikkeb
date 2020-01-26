2b7105f26046917474e4fb76c61725bdc6af758a
jetstreamokayasu <tokotsuku2@outlook.jp>
2020-01-26 22:59:15 +0900

    AIキャリアドラフトスライド用グラフを作成

--- new-okayasu/additional_ex.R
+++ new-okayasu/additional_ex.R
@@ -45,4 +45,59 @@ ahiru.pca<-prcomp(ahiru.mat)
 plot3d(ahiru.pca[["x"]][,1:3])
 text3d(ahiru.pca[["x"]][,1:3], texts = 1:72)
 plot(ahiru.pca[["x"]][,1:2])
-rgl.snapshot("./data/ahiru3d_2.png")  
+rgl.snapshot("./data/ahiru3d_2.png")
+
+plot(ahiru.pca[["x"]][,1:2], col=rainbow(12)[(row(ahiru.pca[["x"]][, 1:2])[, 1]-1) %/% 6 + 1], cex=1.5, pch=16)
+
+# for(i in 0:11){
+#   
+#   points(ahiru.pca[["x"]][(i*6):((i+1)*6), 1:2], col=rainbow(12)[i+1], cex=1.5, pch=16)
+#   debugText(i, rainbow(12)[i])
+# }
+rgl.snapshot("./data/ahiru3d_3.png")
+
+plot3d(ahiru.pca[["x"]][,1:3], type="n")
+spheres3d(ahiru.pca[["x"]][, 1:3], col=rainbow(12)[(row(ahiru.pca[["x"]][, 1:3])[, 1]-1) %/% 6 + 1] , radius = 0.3)
+
+for(i in 0:11){
+  
+  #points3d(ahiru.pca[["x"]][(i*6):((i+1)*6), 1:3], col=rainbow(12)[i+1], size=10, pch=16)
+  spheres3d(ahiru.pca[["x"]][(i*6):((i+1)*6), 1:3], col=rainbow(12)[i+1], radius = 0.3)
+
+}
+
+#PCAの逆写像の際に平均点ではなく中心点を足した場合の図示
+rad<-seq(0, 2*pi, length=20)
+plot(cos(rad[6:14]), sin(rad[6:14]), pch=16, cex=2, xlim=c(-1.5, 1.5), ylim=c(-1.5, 1.5))
+points(mean(cos(rad[6:14])), mean(sin(rad[6:14])), col=2, pch=16, cex=2)
+points(cos(rad[10]), sin(rad[10]), col=4, pch=16, cex=2)
+
+tst_pca=prcomp(cbind(cos(rad[6:14]), sin(rad[6:14])))
+
+#平均点を使った場合の逆写像
+points(tst_pca[["x"]][,1]%*%t(tst_pca[["rotation"]][,1])+matrix(rep(tst_pca[["center"]], times=9), 9, 2,byrow=T), col=3, pch=16)
+abline((-tst_pca[["rotation"]][2,1]/tst_pca[["rotation"]][1,1])*tst_pca[["center"]][1]+tst_pca[["center"]][2], tst_pca[["rotation"]][2,1]/tst_pca[["rotation"]][1,1], lwd=2, col=3)
+
+#中心点を使った場合の逆写像
+points(tst_pca[["x"]][,1]%*%t(tst_pca[["rotation"]][,1])+matrix(rep(c(cos(rad[10]), sin(rad[10])), times=9), 9, 2,byrow=T), col=3, pch=16)
+abline((-tst_pca[["rotation"]][2,1]/tst_pca[["rotation"]][1,1])*cos(rad[10])+sin(rad[10]), tst_pca[["rotation"]][2,1]/tst_pca[["rotation"]][1,1], lwd=2, col=3)
+
+
+theta<-seq(0, 2*pi, length=72)
+
+
+
+set.seed(102)
+nois<-matrix(rnorm(72*2, 0, 0.1), nrow = 72, ncol = 2)
+
+
+library(RDRToolbox)
+d_isomap <- Isomap(data=d_sr, dims=2, k=5)
+plotDR(data=d_isomap[[1]], labels=labels, col=color)
+
+
+library(vegan)
+ahiru_dist<-dist(ahiru.mat)
+
+ahiru_iso <- vegan::isomap(ahiru_dist, ndim = 2, k=5)
+plot(ahiru_iso[[1]], cex=1.5, pch=16, col=rainbow(12)[row(ahiru_iso[[1]])[, 1] %/% 6 + 1 ])
