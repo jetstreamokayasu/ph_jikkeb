7f2212a3b392deae0b9d3ef37d9544d888fc82a2
jetstreamokayasu <tokotsuku2@outlook.jp>
2020-02-08 01:43:10 +0900

    補間された点のみ減らすことを試す

--- new-okayasu/gtm.R
+++ new-okayasu/gtm.R
@@ -112,8 +112,8 @@ trs350_incolle_set1<-gtm_inter_reduce(collect = torus350_colle_set[[1]], nvic = 
   save2Rdata(trs350_incolle_set1_test_aggr)
 }
 
-##並列化を試す
-##350点トーラス
+#並列化準備
+#parallel使用
 library(parallel)
 
 cl <- makeCluster(4, outfile="")
@@ -137,6 +137,11 @@ clusterEvalQ(cl, {
   }
   )
 
+
+stopCluster(cl)
+
+##並列化を試す
+##350点トーラス
 int_time<-system.time(trs350_incolle_set1b<-parLapply(cl, torus350_colle_set[[1]], function(X){
   
   inter_oricord<-voronoi_gtm_interpo(X[[2]], nvics = 30)
@@ -151,7 +156,6 @@ int_time<-system.time(trs350_incolle_set1b<-parLapply(cl, torus350_colle_set[[1]
   
 }))
 
-stopCluster(cl)
 
 #parallelで補間後ベッチ数推定
 ##350点トーラス
@@ -162,29 +166,8 @@ stopCluster(cl)
 
 
 ##並列化を試す
-##350点トーラス
-library(parallel)
+##300点トーラス
 
-cl <- makeCluster(4, outfile="")
-
-clusterEvalQ(cl,{
-  library(phacm)
-  library(interpo3d)
-  library(pracma)
-  library(deldir)
-  library(gtm)
-  library(tidyverse)
-  library(myfs)
-})
-
-clusterEvalQ(cl, {
-  source('~/R/interpolation_test/interpo_func.R', encoding = 'UTF-8')
-  source('~/R/interpolation_test/interpo_func_2.R', encoding = 'UTF-8')
-  source('~/R/interpolation_test/interpo_func.R', encoding = 'UTF-8')
-  source('~/R/p_reduce/reduce_func.R', encoding = 'UTF-8')
-  source('~/R/interpolation_test/reduce_func2.R', encoding = 'UTF-8')
-}
-)
 
 int_time300p<-system.time(trs300_incolle_set1c<-parLapply(cl, torus300_colle_set[[1]], function(X){
   
@@ -200,8 +183,39 @@ int_time300p<-system.time(trs300_incolle_set1c<-parLapply(cl, torus300_colle_set
   
 }))
 
-stopCluster(cl)
 
+
+#parallelで補間後ベッチ数推定
+##300点トーラス
+{
+  trs300_incolle_set1C_test_aggr<-proposedMethodOnly(trs300_incolle_set1c, 2, 3, 10)
+  save2Rdata(trs300_incolle_set1C_test_aggr)
+}
+
+#parallelで補間
+#元のデータ点はすべて残し、補間された点のみ減らす
+#300点トーラス
+
+int_time300p2<-system.time(trs300_incolle_set1D<-parLapply(cl, torus300_colle_set[[1]], function(X){
+  
+  inter_oricord<-voronoi_gtm_interpo(X[[2]], nvics = 30)
+  inter_oricord<-inter_oricord[!is.na(inter_oricord[,1]), ]
+  red_oricord<-reduce_intered2(intered_X = inter_oricord, ratio = 0.99)
+  X[[2]]<-rbind(X[[2]], red_oricord)
+  X[[1]]<-nrow(X[[2]])
+  sink(paste0("./parallel/", X[[1]], "data", format(Sys.time(), "%m%d_%H%M"), ".txt"))
+  print(paste("dataset has", X[[1]], "points"))
+  sink()
+  return(X)
+  
+}))
+
+#parallelで補間後ベッチ数推定
+##300点トーラス
+{
+  trs300_incolle_set1D_test_aggr<-proposedMethodOnly(trs300_incolle_set1D, 2, 3, 10)
+  save2Rdata(trs300_incolle_set1D_test_aggr)
+}
 
 
 ##不具合チェック
